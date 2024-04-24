af91fb63c13b3bc570376d1ad525351feec06db1
desktop <haydenguyn@gmail.com>
2024-04-24 16:18:52 -0700

    Added boilerplate for a table view in the main window

--- money_tracker.py
+++ money_tracker.py
@@ -1,24 +1,51 @@
 import sys
 
 from typing import Union
-from PySide2.QtWidgets import QMainWindow, QApplication, QWidget
+from PySide2.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem
 
-# Creates the UI
 class Money_Tracker(QMainWindow):
-    def __init__(self):
+    def __init__(self): # Initialises the main window for the UI
         super().__init__()
         
         self.setWindowTitle("Money Tracker")
-        self.setGeometry(100, 100, 500, 500)
+        self.setGeometry(700, 300, 500, 500) # x, y, width, height
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
 
+        self.setup_ui()
+
+    # Creates the graphical elements of the UI
+    def setup_ui(self):
+
+        table = QTableWidget(2, 2) # Create a 2x2 table
+        table.setHorizontalHeaderLabels(["Price", "Category"]) # Sets column header lable
+        table.setVerticalHeaderLabels(["ITEM_NAME", "ITEM_NAME"]) # Sets row lable
+
+        item1 = QTableWidgetItem("1000")  # Placeholder item
+        item2 = QTableWidgetItem("50")  # Placeholder item
+        item3 = QTableWidgetItem("RENT")  # Placeholder item
+        item4 = QTableWidgetItem("PHONE")  # Placeholder item
+
+        table.setItem(0, 0, item1) # Placeholder item
+        table.setItem(1, 0, item2) # Placeholder item
+        table.setItem(0, 1, item3) # Placeholder item
+        table.setItem(1, 1, item4) # Placeholder item
+
+        table.setColumnWidth(0, 100)
+        table.setColumnWidth(1, 150)
+        
+        # Creates a vertical layout and adds the table widget to it
+        layout = QVBoxLayout()
+        layout.addWidget(table)
+        self.central_widget.setLayout(layout)
+
+
 class Item:
-    def __init__(self, name: str, price: Union[int, float], category: str):
+    def __init__(self, name: str, price: Union[int, float], category: str): # Define paramaters of a certain type. Union[] allows either option
         self.name = name
         self.price = price
-        self.valid_category(category)
+        self.valid_category(category) # Runs the valid category function on the category and sets self.category if valid
         self.category = category
 
     # Check that the category is in the list of valid options and return ValueError if not
