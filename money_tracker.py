110eb85f91d19efcc6aa6572dd094598aa6967f8
desktop <haydenguyn@gmail.com>
2024-04-24 15:14:38 -0700

    Added Union method to allow for floats or ints in paramaters. Added a valid category function to check if the category is valid

--- money_tracker.py
+++ money_tracker.py
@@ -1,9 +1,20 @@
 from typing import Union
 
-class Item():
-    def __init__(self, name: str, price: Union[int, float]):
+class Item:
+    def __init__(self, name: str, price: Union[int, float], category: str):
         self.name = name
         self.price = price
+        self.valid_category(category)
+        self.category = category
 
-test_item = Item("Food", 50.0)
-test_item2 = Item("Phone", 27)
+    # Check that the category is in the list of valid options and return ValueError if not
+    def valid_category(self, category: str):
+        valid_options = ("RENT", "GROCERIES", "BILLS", "SUBSCRIPTIONS", "HOUSEHOLD", "RESTAURANTS", "ENTERTAINMENT", "OTHER")
+        if category not in valid_options:
+            raise ValueError(f"Invalid category: {category}\nValid categories: {valid_options}")
+
+
+test_item = Item("Food", 50.0, "GROCERIES")
+test_item2 = Item("Phone", 27, "BILLS")
+print(test_item.category)
+print(test_item2)
