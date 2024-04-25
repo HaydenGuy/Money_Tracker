e6f600622ab90e4df3d3a866d8ad1b5520687624
desktop <haydenguyn@gmail.com>
2024-04-25 12:58:05 -0700

    Removed the category validation as the user will select this from the UI

--- money_tracker.py
+++ money_tracker.py
@@ -5,21 +5,9 @@ from PySide2.QtWidgets import QApplication
 from modules import UI # Imports the UI module
 
 class Item:
-    def __init__(self, name: str, price: Union[int, float], category: str): # Define paramaters of a certain type. Union[] allows either option
+    def __init__(self, name: str, price: Union[int, float]): # Define paramaters of a certain type. Union[] allows either option
         self.name = name
         self.price = price
-        self.valid_category(category) # Runs the valid category function on the category and sets self.category if valid
-        self.category = category
-
-    # Check that the category is in the list of valid options and return ValueError if not
-    def valid_category(self, category: str):
-        valid_options = ("RENT", "GROCERIES", "BILLS", "SUBSCRIPTIONS", "HOUSEHOLD", "RESTAURANTS", "ENTERTAINMENT", "OTHER")
-        if category not in valid_options:
-            raise ValueError(f"Invalid category: {category}\nValid categories: {valid_options}")
-
-
-test_item = Item("Food", 50.0, "GROCERIES")
-test_item2 = Item("Phone", 27, "BILLS")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
