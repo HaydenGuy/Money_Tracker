ffb34f3e187533e769b931e09c074a5c7fe1ae60
desktop <haydenguyn@gmail.com>
2024-04-25 15:29:50 -0700

    Moved the item class to the UI module

--- money_tracker.py
+++ money_tracker.py
@@ -1,13 +1,7 @@
 import sys
 
-from typing import Union
 from PySide2.QtWidgets import QApplication
 from modules import UI # Imports the UI module
-
-class Item:
-    def __init__(self, name: str, price: Union[int, float]): # Define paramaters of a certain type. Union[] allows either option
-        self.name = name
-        self.price = price
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
